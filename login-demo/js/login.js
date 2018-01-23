7e7f588cd8cd1cd7f06ee86298f7929bf8a35938
dreamerlxb <1558485399@qq.com>
2018-01-23 17:20:42 +0800

    add calendar

--- login-demo/js/login.js
+++ login-demo/js/login.js
@@ -1,66 +1,73 @@
-$(function(){
-	
-	$('#switch_qlogin').click(function(){
+$(function () {
+
+	$('#switch_qlogin').click(function () {
 		$('#switch_login').removeClass("switch_btn_focus").addClass('switch_btn');
 		$('#switch_qlogin').removeClass("switch_btn").addClass('switch_btn_focus');
-		$('#switch_bottom').animate({left:'0px',width:'70px'});
-		$('#qlogin').css('display','none');
-		$('#web_qr_login').css('display','block');
-		
+		$('#switch_bottom').animate({
+			left: '0px',
+			width: '70px'
 		});
-	$('#switch_login').click(function(){
-		
-		$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
-		$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
-		$('#switch_bottom').animate({left:'154px',width:'70px'});
-		
-		$('#qlogin').css('display','block');
-		$('#web_qr_login').css('display','none');
-		});
-if(getParam("a")=='0')
-{
-	$('#switch_login').trigger('click');
-}
+		$('#qlogin').css('display', 'none');
+		$('#web_qr_login').css('display', 'block');
 
 	});
-	
-function logintab(){
+	$('#switch_login').click(function () {
+
+		$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
+		$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
+		$('#switch_bottom').animate({
+			left: '154px',
+			width: '70px'
+		});
+
+		$('#qlogin').css('display', 'block');
+		$('#web_qr_login').css('display', 'none');
+	});
+	if (getParam("a") == '0') {
+		$('#switch_login').trigger('click');
+	}
+
+});
+
+function logintab() {
 	scrollTo(0);
 	$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
 	$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
-	$('#switch_bottom').animate({left:'154px',width:'96px'});
-	$('#qlogin').css('display','none');
-	$('#web_qr_login').css('display','block');
-	
+	$('#switch_bottom').animate({
+		left: '154px',
+		width: '96px'
+	});
+	$('#qlogin').css('display', 'none');
+	$('#web_qr_login').css('display', 'block');
+
 }
 
 
 //根据参数名获得该参数 pname等于想要的参数名 
-function getParam(pname) { 
-    var params = location.search.substr(1); // 获取参数 平且去掉？ 
-    var ArrParam = params.split('&'); 
-    if (ArrParam.length == 1) { 
-        //只有一个参数的情况 
-        return params.split('=')[1]; 
-    } 
-    else { 
-         //多个参数参数的情况 
-        for (var i = 0; i < ArrParam.length; i++) { 
-            if (ArrParam[i].split('=')[0] == pname) { 
-                return ArrParam[i].split('=')[1]; 
-            } 
-        } 
-    } 
-}  
+function getParam(pname) {
+	var params = location.search.substr(1); // 获取参数 平且去掉？ 
+	var ArrParam = params.split('&');
+	if (ArrParam.length == 1) {
+		//只有一个参数的情况 
+		return params.split('=')[1];
+	} else {
+		//多个参数参数的情况 
+		for (var i = 0; i < ArrParam.length; i++) {
+			if (ArrParam[i].split('=')[0] == pname) {
+				return ArrParam[i].split('=')[1];
+			}
+		}
+	}
+}
 
 
 var reMethod = "GET",
 	pwdmin = 6;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-	$('#reg').click(function() {
+	$('#reg').click(function () {
 
 		if ($('#user').val() == "") {
 			$('#user').focus().css({
@@ -88,13 +95,14 @@ $(document).ready(function() {
 			url: "/member/ajaxyz.php",
 			data: "uid=" + $("#user").val() + '&temp=' + new Date(),
 			dataType: 'html',
-			success: function(result) {
+			success: function (result) {
 
 				if (result.length > 2) {
 					$('#user').focus().css({
 						border: "1px solid red",
 						boxShadow: "0 0 2px red"
-					});$("#userCue").html(result);
+					});
+					$("#userCue").html(result);
 					return false;
 				} else {
 					$('#user').css({
@@ -124,17 +132,18 @@ $(document).ready(function() {
 				border: "1px solid red",
 				boxShadow: "0 0 2px red"
 			});
-			$('#userCue').html("<font color='red'><b>×QQ号码格式不正确</b></font>");return false;
+			$('#userCue').html("<font color='red'><b>×QQ号码格式不正确</b></font>");
+			return false;
 		} else {
 			$('#qq').css({
 				border: "1px solid #D7D7D7",
 				boxShadow: "none"
 			});
-			
+
 		}
 
 		$('#regUser').submit();
 	});
-	
+
 
 });
--- mobile-ui/calendar/index.js
+++ mobile-ui/calendar/index.js
@@ -1,3 +1,124 @@
-(function () {  
+// (function () {
+   function Calendar(dom) {
+        this.container = dom;
+
+        this.header = document.createElement("div");
+        // 表格区 显示数据
+        this.body = document.createElement("div");
+        // this.footer = document.createElement("div");
+
+        this.calendarStart = new Date();
+        this.calendarEnd = new Date();
+
+        this.current = new Date();
+
+        this.__calData__();
+        this.createHeaderHtml();
+        this.createBodyHtml();
+
+       // this.container.appendChild(this.footer);
+   }
+
+   Calendar.prototype.createHeaderHtml = function() {
+        this.header.className = 'calendar-title-box';     // 设置标题盒子中的html
+        this.header.innerHTML = "" +
+            "<span class='prev-month' id='prevMonth'></span>" +       
+            "<span class='calendar-title' id='calendarYear'> " + this.current.getFullYear() + "</span>" + // year
+            "<span class='calendar-title' id='calendarMonth'>" + (this.current.getMonth() + 1) + "</span>" + // month
+            "<span id='nextMonth' class='next-month'></span>";
+
+        this.container.appendChild(this.header);
+   };
    
-})();
+   Calendar.prototype.createBodyHtml = function() {
+        // 设置表格区的html结构 
+        this.body.className = 'calendar-body-box';    
+        var _headHtml = "" +
+            "<thead>" +
+                "<tr>" +
+                    "<th>一</th>" +
+                    "<th>二</th>" +
+                    "<th>三</th>" +
+                    "<th>四</th>" +
+                    "<th>五</th>" +
+                    "<th>六</th>" +
+                    "<th>日</th>" +
+                "</tr>" +
+            "</thead>";
+        var _bodyHtml ="<tbody> </tbody>";
+        
+        this.body.innerHTML = "" +
+            "<table id='calendarTable' class='calendar-table'>" +
+                _headHtml +
+                _bodyHtml +
+            "</table>";     // 添加到calendar div中
+
+        this.container.appendChild(this.body);
+        
+   };
+
+   Calendar.prototype.show = function() {
+        this.__createDayHtml__();
+    };
+
+   /**
+    * 共5行 7列 35个数据
+    prevDays： 上月共有几天
+    currentDays： 本月有几天
+    */
+   Calendar.prototype.__createDayHtml__ = function() {
+        var _bodyHtml = "";      // 一个月最多31天，所以一个月最多占6行表格
+        
+        console.log(this.calendarStart.getFullYear() + "-" + (this.calendarStart.getMonth() + 1) + "-" + this.calendarStart.getDate());
+        var tmpDate = new Date();
+        tmpDate.setTime(this.calendarStart.getTime());
+
+
+        for (var i = 0; i < 5; i++) {
+            _bodyHtml += "<tr>";
+            for(var j = 0; j < 7; j++) {
+                tmpDate.setTime(tmpDate.getTime() + 24 * 60 * 60 * 1000);
+
+                if(this.current.getFullYear() === tmpDate.getFullYear() &&
+                    this.current.getMonth() === tmpDate.getMonth() &&
+                    this.current.getDate() === tmpDate.getDate() ) {
+                        _bodyHtml += "<td class='today' data-date='" + (tmpDate.getFullYear() + "-" + (tmpDate.getMonth() + 1) + "-" + tmpDate.getDate()) + "'>" + tmpDate.getDate() + "</td>";
+                    } else {
+                        _bodyHtml += "<td data-date='" + (tmpDate.getFullYear() + "-" + (tmpDate.getMonth() + 1) + "-" + tmpDate.getDate()) + "'>" + tmpDate.getDate() + "</td>";
+                    }
+            }
+            _bodyHtml += "</tr>";
+        }
+
+        this.body.querySelector('table tbody').innerHTML = _bodyHtml;
+   };
+
+   Calendar.prototype.__calData__ = function() {
+        // 下月的第一天
+        var firstDayOfNextMonth = new Date(this.current.getFullYear() + '-' + (this.current.getMonth() + 1 + 1) + '-01');
+        // 当前月的第一天
+        var firstDayOfCurrentMonth = new Date(this.current.getFullYear() + "-" + (this.current.getMonth() + 1) + "-01");
+        // 当前月的最后一天
+        var lastDayOfCurrentMonth = new Date(firstDayOfNextMonth.getTime() - 1 * 24 * 60 * 60 * 1000);
+        // 上月的最后一天
+        var lastDayOfPrevMonth = new Date(firstDayOfCurrentMonth.getTime() - 1 * 24 * 60 * 60 * 1000);
+
+        // 当前月第一天的星期
+        var week1 = firstDayOfCurrentMonth.getDay();
+        // 需要向前一个月借 (week1 - 1)天
+        // 所以日历的开始点为上月的最后一天减去(week1 - 1)天
+        this.calendarStart = new Date(lastDayOfPrevMonth.getTime() - (week1 - 1) * 24 * 60 * 60 * 1000);
+        console.log(this.calendarStart);
+
+        // 当前月最后一天的星期
+        var week2 = lastDayOfCurrentMonth.getDay();
+        // 需要向下一个月借 (7 - week2) 天
+        // 所以日历的结束点为下月的第一天加上(7 - week2)天
+        this.calendarEnd = new Date(firstDayOfNextMonth.getTime() + (7 - week2) * 24 * 60 * 60 * 1000);
+        console.log(this.calendarEnd);
+   };
+
+   Calendar.prototype.__bindEvent__ = function() {
+
+   };
+// })();
